d58caf1a542647ce0ac6232eedd6f356c723e3fb
haroonayeni99-source <haroonayeni99@gmail.com>
2026-09-21 22:07:41 +0100

    Migrate Glohaus authentication to Supabase

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,8 @@
-import { SignIn } from "@clerk/nextjs";
 import { authConfigured } from "@/lib/config";
 import { AuthFrame } from "@/components/auth-frame";
 import { AccessMessage } from "@/components/access-message";
 import { safeReturnTo } from "@/lib/return-to";
+import { EmailAuthForm } from "@/components/email-auth-form";
 export const metadata = { title: "Sign in" };
 export default async function Page({
   searchParams,
@@ -13,11 +13,7 @@ export default async function Page({
   return (
     <AuthFrame>
       {authConfigured() ? (
-        <SignIn
-          routing="path"
-          path="/sign-in"
-          forceRedirectUrl={safeReturnTo(returnTo)}
-        />
+        <EmailAuthForm mode="sign-in" redirectTo={safeReturnTo(returnTo)} />
       ) : (
         <AccessMessage />
       )}

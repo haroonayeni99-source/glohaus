425c063c8878ccc57e751a545ca82a3e40b12f80
haroonayeni99-source <haroonayeni99@gmail.com>
2026-09-25 21:44:30 +0100

    Keep Discover sharing and navigation context correct

--- src/components/discovery-feed.tsx
+++ src/components/discovery-feed.tsx
@@ -446,6 +446,7 @@ export function DiscoveryFeed({
                       requireAuth={
                         accountMode ? undefined : requireAuthForEngagement
                       }
+                      editorialBase={routeBase}
                     />
                   </article>
                 ))}
@@ -619,7 +620,7 @@ export function DiscoveryFeed({
           </div>
         )}
       </main>
-      <BottomNavigation active="home" />
+      <BottomNavigation />
     </div>
   );
 }
